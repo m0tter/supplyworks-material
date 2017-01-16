633b7b0c8e972c0f831845d8972d5ebaac4dedfe
m0tter <qldscottie@gmail.com>
2017-01-16 19:14:58 +1000

    del

--- dist/server/routes/default.route.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var userAPI = require("../api/school/user.api");
-function registerRoutes(app) {
-    app.use('/api/school/users', userAPI.apiController());
-}
-exports.registerRoutes = registerRoutes;
