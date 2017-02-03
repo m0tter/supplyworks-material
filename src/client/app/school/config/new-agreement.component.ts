b3a5184428d7f68776c21ba6c27d3ad3610db309
m0tter <qldscottie@gmail.com>
2017-02-03 16:41:36 +1000

    new agreement

---
+++ src/client/app/school/config/new-agreement.component.ts
@@ -0,0 +1,5 @@
+import { Component, OnInit, Optional }  from '@angular/core';
+import { MdDialogRef }                  from '@angular/material';
+import { Agreement }                    from 'supplyworks';
+
+
